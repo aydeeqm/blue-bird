a7fc3d1236489ce7455406bd3092a1ed083e45d8
Aydee-QM <aydee.qm@gmail.com>
2019-11-05 12:35:40 -0500

    prepare dev

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render('hola mundo', document.getElementById('app'));
+ReactDOM.render(<h1>hola mundooooooooo</h1>, document.getElementById('app'))
