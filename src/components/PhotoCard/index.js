a160109253abe9fe31c7d5dd91f68018c56e4f67
Aydee-QM <aydee.qm@gmail.com>
2019-11-18 15:21:49 -0500

    add custom hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,29 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const elementRef = useRef(null)
-  const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [show, elementRef] = useNearScreen()
+  const [liked, setLiked] = useLocalStorage(key, false)
 
-  useEffect(() => {
-    Promise.resolve(
-      typeof window.IntersectionObserver === 'undefined'
-        ? import('intersection-observer')
-        : window.IntersectionObserver
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(elementRef.current)
-    })
-  }, [elementRef])
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={elementRef}>
@@ -34,8 +22,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button>
-            <MdFavoriteBorder size='32px' /> {likes} likes!
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size='32px' /> {likes} likes!
           </Button>
         </>
 
