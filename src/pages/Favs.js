e4d30d86569041e0f6897c3ad89a0539b129fc27
Aydee-QM <aydee.qm@gmail.com>
2020-01-24 18:15:23 -0500

    add react-helmet to custom metadata

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { FavsWithQuery } from '../container/GetFavorites'
 
 export const Favs = () => {
   return (
     <>
+      <Helmet>
+        <title>BlueBird - Tu favoritos</title>
+        <meta name='descripcion' content='Aquí puedes encontrar tus favoritos' />
+      </Helmet>
       <h1>Favs</h1>
       <FavsWithQuery />
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 
 export const Home = ({ categoryId }) => {
   return (
     <>
+      <Helmet>
+        <title>BlueBird - Tu app de fotos de mascotas</title>
+        <meta name='descripcion' content='Con BlueBird puedes encontrar fotos de animales domésticos muy bonitos' />
+      </Helmet>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={categoryId} />
     </>
